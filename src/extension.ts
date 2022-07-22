9de18414711fb3aef08314f164e181272300a89e
phy-lei <765373325@qq.com>
2022-07-22 18:13:20 +0800

    fix: 修复保存json文件时，也执行api2ts更新的操作bug

--- src/extension.ts
+++ src/extension.ts
@@ -54,12 +54,13 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.workspace.onDidSaveTextDocument(({ languageId, fileName }) => {
       // 过滤非 Json 语言文件 且非配置文件
-      if (languageId !== 'json' && fileName !== workspaceConfigPath)
-        return;
+      if (languageId !== 'json' || fileName !== workspaceConfigPath)
+        return
+
       vscode.commands.executeCommand('Api2ts.api2tsUpdate');
     })
   );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
